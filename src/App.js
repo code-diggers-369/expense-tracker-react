e3140550fa4793107cc7f6ad1ea441a7a4ba9f23
code-diggers-369 <codediggers007@gmail.com>
2021-11-13 18:18:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,201 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
+  const tableData = {
+    Months: [
+      "Apr 2021",
+      "May 2021",
+      "Jun 2021",
+      "Jul 2021",
+      "Aug 2021",
+      "Sep 2021",
+      "Oct 2021",
+      "Nov 2021",
+      "Dec 2021",
+      "Jan 2022",
+      "Feb 2022",
+      "Mar 2022",
+    ],
+    Expense: ["Labor", "Materials", "Subcontractor", "Job Costing", "Lodging"],
+  };
+
+  const handleTableChanges = (rowIndex, columnIndex, value) => {
+    try {
+      const columnTotalSelector = document.getElementById(
+        `totalcol-${columnIndex}`
+      );
+      const rowTotalSelector = document.getElementById(`totalrow-${rowIndex}`);
+      const mainTotalSelector = document.getElementById("main-total");
+
+      const { columnTotal, rowTotal } = getColumnListTotal(
+        rowIndex,
+        columnIndex
+      );
+
+      columnTotalSelector.textContent = columnTotal;
+      rowTotalSelector.textContent = rowTotal;
+
+      const finalColumnTotal = getSubTotal();
+
+      mainTotalSelector.textContent = finalColumnTotal;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getSubTotal = () => {
+    try {
+      var finalColumnTotal = 0;
+
+      for (let i = 0; i < tableData.Months.length; i++) {
+        const totalColumnData = document.getElementById(`totalcol-${i}`);
+
+        const totalColumnValue = parseInt(totalColumnData.textContent)
+          ? parseInt(totalColumnData.textContent)
+          : 0;
+        finalColumnTotal += totalColumnValue;
+      }
+
+      return finalColumnTotal;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getColumnListTotal = (rowIndex, columnIndex) => {
+    try {
+      var rowTotal = 0;
+      var columnTotal = 0;
+
+      for (let i = 0; i < 12; i++) {
+        const rowData = document.getElementById(`row-${rowIndex}-col-${i}`)
+          .children[0];
+
+        const rowValue = parseInt(rowData.value) ? parseInt(rowData.value) : 0;
+        rowTotal += rowValue;
+      }
+
+      for (let i = 0; i < tableData.Expense.length; i++) {
+        const columnData = document.getElementById(
+          `row-${i}-col-${columnIndex}`
+        ).children[0];
+        const columnValue = parseInt(columnData.value)
+          ? parseInt(columnData.value)
+          : 0;
+        columnTotal += columnValue;
+      }
+
+      return {
+        rowTotal,
+        columnTotal,
+      };
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className=" mt-5">
+        <table className="table table-bordered ">
+          <thead>
+            <tr>
+              <th className="category-header text-center">Category</th>
+              {tableData.Months.map((list, index) => {
+                return (
+                  <th className="text-right" style={{ width: 100 }} key={index}>
+                    {list}
+                  </th>
+                );
+              })}
+
+              <th className="text-right text-center" style={{ width: 100 }}>
+                Total
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.Expense.map((list, rowIndex) => {
+              return (
+                <tr key={rowIndex} id={`row-${rowIndex}`}>
+                  <td className="">{list}</td>
+                  {tableData.Months.map((ls, colIndex) => {
+                    return (
+                      <td
+                        className="amount-column"
+                        key={colIndex}
+                        id={`row-${rowIndex}-col-${colIndex}`}
+                      >
+                        <input
+                          className="form-control cursor-pointer"
+                          type="number"
+                          onChange={(e) =>
+                            handleTableChanges(
+                              rowIndex,
+                              colIndex,
+                              e.target.value
+                            )
+                          }
+                        />
+                      </td>
+                    );
+                  })}
+                  <td
+                    className="text-center budget-total ember-view"
+                    id={`totalrow-${rowIndex}`}
+                  >
+                    <span className="form-control-static">0</span>
+                  </td>
+                </tr>
+              );
+            })}
+
+            {/**/}
+            <tr className="text-center" id="final-total">
+              <td className="text-overflow">Total</td>
+              <td id="totalcol-0" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="totalcol-1" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="totalcol-2" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="totalcol-3" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="totalcol-4" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="totalcol-5" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="totalcol-6" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="totalcol-7" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="totalcol-8" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="totalcol-9" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="totalcol-10" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="totalcol-11" className="text-right">
+                <span className="">0</span>
+              </td>
+              <td id="main-total" className="text-right">
+                <span className="">0</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
-
-export default App;
